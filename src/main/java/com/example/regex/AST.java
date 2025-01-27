06bb4b34a0d29312f2b54733159d4d29b9c9388d
Saksham Gupta <sakshamdam54321@gmail.com>
2025-01-27 23:55:19 +0530

    don't need to copy Immutable Collection

--- src/main/java/com/example/regex/AST.java
+++ src/main/java/com/example/regex/AST.java
@@ -12,11 +12,6 @@ record Expression(List<ASTNode> children) implements ASTNode {
     Expression {
         children = List.copyOf(children);
     }
-
-    public List<ASTNode> children() {
-        return List.copyOf(this.children);
-    }
-
 }
 
 record StringNode(String value) implements ASTNode {
@@ -25,9 +20,5 @@ record StringNode(String value) implements ASTNode {
 record Alternation(List<ASTNode> options) implements ASTNode {
     Alternation {
         options = List.copyOf(options);
-    }
-
-    public List<ASTNode> options() {
-        return List.copyOf(this.options);
     }
 }

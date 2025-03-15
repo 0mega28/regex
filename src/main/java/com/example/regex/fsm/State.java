f21eff2c6e5c77c208cec69ccda2c05f168b1fdf
Saksham Gupta <sakshamdam54321@gmail.com>
2025-03-15 20:19:23 +0530

    remove default methods

--- src/main/java/com/example/regex/fsm/State.java
+++ src/main/java/com/example/regex/fsm/State.java
@@ -3,7 +3,6 @@ package com.example.regex.fsm;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public final class State {
     private final List<Transition> transitions;
@@ -24,24 +23,4 @@ public final class State {
         this.transitions.clear();
         this.transitions.addAll(transitions);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (State) obj;
-        return Objects.equals(this.transitions, that.transitions);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(transitions);
-    }
-
-    @Override
-    public String toString() {
-        return "State[" +
-               "transitions=" + transitions + ']';
-    }
-
 }
